058a431e8761dd9c67f05f9e941a9e93d52f8c56
haoyuhhuang <haoyuhhuang@tencent.com>
2023-05-23 15:06:15 +0800

    feat: use template generate component

--- BookCard.js
+++ BookCard.js
@@ -2,28 +2,33 @@ class BookCard extends HTMLElement {
     constructor() {
         super()
 
-        const container = document.createElement('div')
-        container.className = 'container'
+        // 通过生成DOM元素方式
+        // const container = document.createElement('div')
+        // container.className = 'container'
+        //
+        // const image = document.createElement('img')
+        // image.className = 'image'
+        // image.src = "https://file.ituring.com.cn/LargeCover/23047b0f0134221867e2"
+        //
+        // const title = document.createElement('p')
+        // title.className = 'title'
+        // title.textContent = 'Python编程：从入门到实践（第3版）'
+        //
+        // const author = document.createElement('p')
+        // author.className = 'author'
+        // author.textContent = '[美]埃里克·马瑟斯（Eric Matthes）'
+        //
+        // const translator = document.createElement('p')
+        // translator.className = 'translator'
+        // translator.textContent = '袁国忠'
+        //
+        // container.append(image, title, author, translator)
 
-        const image = document.createElement('img')
-        image.className = 'image'
-        image.src = "https://file.ituring.com.cn/LargeCover/23047b0f0134221867e2"
+        // 通过注册组件调用该模板
+        const templateElem = document.getElementById('book-card-template')
+        const clonedElem = templateElem.content.cloneNode(true)
 
-        const title = document.createElement('p')
-        title.className = 'title'
-        title.textContent = 'Python编程：从入门到实践（第3版）'
-
-        const author = document.createElement('p')
-        author.className = 'author'
-        author.textContent = '[美]埃里克·马瑟斯（Eric Matthes）'
-
-        const translator = document.createElement('p')
-        translator.className = 'translator'
-        translator.textContent = '袁国忠'
-        
-        container.append(image, title, author, translator)
-
-        this.appendChild(container)
+        this.appendChild(clonedElem)
     }
 }
 
